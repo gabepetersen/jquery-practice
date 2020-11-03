96957de3b21147a0eb33f4a466597f11b8b94e66
gabepetersen <precariousplant@gmail.com>
2020-11-03 14:27:04 -0800

    feat: add selectors section

--- client/index.js
+++ client/index.js
@@ -1,7 +1,20 @@
 
 function highlight(element) {
-  $(element).addClass('highlighted');
-  setTimeout(function() {
-    $(element).removeClass('highlighted');
-  }, 1000)
+  if($(element)) {
+    $(element).addClass('highlighted');
+    setTimeout(function() {
+      $(element).removeClass('highlighted');
+    }, 2000);
+  } else {
+    alert("passed element is not valid");
+  }
+}
+
+function readyJquery() {
+  $("*").on("click", function(event) {
+    $('.pageclick').text("Clicked at " + event.pageX + ", " + event.pageY + " pixels");
+  })
+}
+function readyWindow() {
+  // maybe do something here in the future
 }
