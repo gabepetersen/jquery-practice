a3bb1cfdbd2e78e22d26d2ce19e407d30f6f2a69
gabepetersen <precariousplant@gmail.com>
2020-11-03 16:20:07 -0800

    feat: add event section

--- client/index.js
+++ client/index.js
@@ -11,10 +11,38 @@ function highlight(element) {
 }
 
 function readyJquery() {
-  $("*").on("click", function(event) {
-    $('.pageclick').text("Clicked at " + event.pageX + ", " + event.pageY + " pixels");
-  })
+  bindEvents();
+  
 }
-function readyWindow() {
-  // maybe do something here in the future
+function bindEvents() {
+  // .on events
+  $('*').on('click', function(event) {
+    $('#pageclick').text(event.pageX + ", " + event.pageY + " pixels");
+  });
+  $('#hoversection').on('mouseenter', function() {
+    $('#mousehoverlisten').text("Mouse Entered");
+  });
+  $('#hoversection').on('mouseleave', function() {
+    $('#mousehoverlisten').text("Mouse Left");
+  });
+  $(window).on('resize', function() {
+    $('#resizelisten').text('Window size: ' + window.innerWidth + ", " + window.innerHeight);
+  });
+  $(window).on('scroll', function() {
+    $('#scrolllisten').text('Scroll Position: ' + window.pageYOffset)
+  });
+  $('#keyinput').on('keypress', function(event) {
+    $('#keypresslisten').text('Keypress: ' + event.originalEvent.key);
+  });
+  $('#clicksection').on('mousedown', function() {
+    $('#mousepresslisten').text('Mouse Pressed Down');
+  });
+  $('#clicksection').on('mouseup', function() {
+    $('#mousepresslisten').text('Mouse Released');
+  })
+  
+  
+}
+function readyWindow(windowRef) {
+  // .resize event
 }
